22c74ce1e3054c8cd25f005ff2eb5cf85546d932
RenePadua <rene.padua@gmail.com>
2024-11-18 19:28:50 -0300

    Alterações para responsividade em dispositivo Mobile

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,2 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dataFixa = new Date(1984, 2, 27);
-
-    const dataAtual = new Date();
-
-    // Calcula a diferença em milissegundos
-    const diferencaEmMilissegundos = dataAtual - dataFixa;
-
-    // Converte para anos
-    const Anos = Math.floor(diferencaEmMilissegundos / (1000 * 60 * 60 * 24 * 365.25));
-
-    // Exibe o resultado no HTML
-    document.getElementById("nascimento").textContent = `${Anos} anos`;
-});
-
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
