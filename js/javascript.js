44003fb9148cc286d412cbe8d69b56bc22d10075
RenePadua <rene.padua@gmail.com>
2024-11-28 16:30:16 -0300

    Responsividade de diversos tamanhos acrescentados; Reajustado o botão de compartilhamento do site e reajustado os links e cores do conjunto de email, whatsapp, linkedin e github

--- js/javascript.js
+++ js/javascript.js
@@ -1,2 +1,27 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+
+if (navigator.share) {
+    console.log("Web Share API é suportada!");
+} else {
+    console.log("Web Share API não é suportada neste navegador.");
+}
+
+const shareButtons = document.getElementsByClassName('shareButton');
+
+// Handle multiple elements with the same class
+for (let i = 0; i < shareButtons.length; i++) {
+    shareButtons[i].addEventListener('click', function() {
+        if (navigator.share) {
+            navigator.share({
+                title: 'Confira este site interessante!',
+                text: 'Trata-se de um <i>curriculum vitae</i>:',
+                url: 'https://renepadua.github.io/CV-Online/'
+            })
+            .then(() => console.log('Compartilhamento realizado com sucesso!'))
+            .catch((error) => console.error('Erro ao compartilhar:', error));
+        } else {
+            alert('Compartilhamento não suportado neste navegador.');
+        }
+    });
+}
